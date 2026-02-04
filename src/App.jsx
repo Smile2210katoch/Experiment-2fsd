2d26a117d8bf6d224c4872c54609e6826eb328a1
Smile2210katoch <smilekatoch64@gmail.com>
2026-02-04 15:58:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,6 @@ function App() {
       <NavbarComp />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
